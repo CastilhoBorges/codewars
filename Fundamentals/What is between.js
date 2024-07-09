a6e547203c5d311feb44d2f2822e2e882e18f53b
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-07-09 15:26:50 -0300

    Grade book and What is between

---
+++ Fundamentals/What is between.js
@@ -0,0 +1,20 @@
+function between1(a, b) {
+  let arr = [];
+  while (a <= b) {
+    arr.push(a);
+    a++;
+  }
+
+  return arr;
+}
+
+function between2(a, b) {
+  let arr = [];
+  for (let princip = a; princip <= b; princip++) {
+    arr.push(princip);
+  }
+
+  return arr;
+}
+
+console.log(between2(1,4));
