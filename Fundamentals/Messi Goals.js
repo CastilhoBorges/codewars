127fc6ff69c2edaf578cb071a85c593dced0a243
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-05 17:11:39 -0300

    Challenges 05/09

---
+++ Fundamentals/Messi Goals.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
---
+++ Fundamentals/Name Shuffler.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const nameShuffler = function (str) {
+  return str.split(" ").reverse().join(" ");
+};
